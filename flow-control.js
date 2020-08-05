ac961f356f55c0f540e575e63014d48889bb9086
Victoria <victoria.eisemann@gmail.com>
2020-08-05 18:06:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,39 @@
 function basicTeenager(age) {
-
+if(age>=13 && age<=19){
+  return("You are a teenager!");
+}
 }
 
 function teenager(age) {
-
+if(age>=13 && age<=19){
+  return("You are a teenager!");
+}
+else {
+    return("You are not a teenager");
+}
 }
 
 function ageChecker(age) {
-
+if(age<13) {
+  return("You are a kid")
+}
+else if(age>19){
+  return("You are a grownup")
+}
+else {
+  return("You are a teenager!")
+}
 }
 
 function ternaryTeenager(age) {
-
+return age>=13 && age<=19 ? "You are a teenager":"You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case 13,14,15,16,17,18,19:
+    return("You are a teenager");
+    default:
+    return("You have an age");
+}
 }
