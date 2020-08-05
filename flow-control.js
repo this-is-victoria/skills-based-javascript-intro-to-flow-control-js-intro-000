d3e2466903a4fa1bb07a29d38512072898bd5280
Victoria <victoria.eisemann@gmail.com>
2020-08-05 18:10:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,7 +31,7 @@ return age>=13 && age<=19 ? "You are a teenager":"You are not a teenager"
 
 function switchAge(age) {
 switch(age) {
-  case age>12 && age<19:
+  case (age>12 && age<19):
     return("You are a teenager");
     default:
     return("You have an age");
